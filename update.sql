aed95bd482050a86fb68492d8075bf36bdfbc52d
Stanislav Kozlov <bleymy@mail.ru>
2015-09-09 14:32:43 +0300

    1st comment

---
+++ update.sql
@@ -0,0 +1,20 @@
+declare
+  result integer;
+/*функция апдейтит  остатки по счету с аккдатой p_idaccdata начиная с даты p_datefrom до конца на p_diff*/
+function updateBalance5acc(p_idaccdata accdata.idaccdata%type, p_datefrom date, p_diff accbal1.accbal%type)
+return integer is
+begin
+  return 1;
+exception
+  when others then
+    return 0;
+end;
+
+begin
+  result := updateBalance5acc(p_idaccdata => , p_datefrom =>, p_diff =>);
+  dbms_output.put_line(result);  
+
+exception
+  when others then
+    null;
+end;
